17f96dcc2a2d8aaea5585d780cd5f9d124275ba5
Deepak Kumar <dk705437@gmail.com>
2024-02-14 02:02:01 +0530

    devs

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
       Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
       </p>
       <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
-      <h3 className='font-bold mb-4 text-slate-800'>Devs : Deepak Kumar | Ashwani Sharma</h3>
+      {/* <h3 className='font-bold mb-4 text-slate-800'>Devs : Deepak Kumar | Ashwani Sharma</h3> */}
     </div>
   )
 }
